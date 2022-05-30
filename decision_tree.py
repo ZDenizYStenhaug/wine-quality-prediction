cd611d00bb32c63f3d9073978ec61ff8b45d3f5c
zeynepDYilmaz <yilmaz.zeynepdeniz@gmail.com>
2022-05-30 09:09:57 +0300

    k-fold for decision tree

--- decision_tree.py
+++ decision_tree.py
@@ -7,23 +7,40 @@ from six import StringIO
 from IPython.display import Image
 import pydotplus
 
+# define column names
+col_names = ['fixed_acidity', 'volatile_acidity', 'citric_acid', 'residual_sugar', 'chlorides',
+             'free_sulfur_dioxide', 'total_sulfur_dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality']
+feature_cols = ['fixed_acidity', 'volatile_acidity', 'citric_acid', 'residual_sugar', 'chlorides',
+                'free_sulfur_dioxide', 'total_sulfur_dioxide', 'density', 'pH', 'sulphates', 'alcohol']
 
-def decision_tree_impl():
-    # define column names
-    col_names = ['fixed_acidity', 'volatile_acidity', 'citric_acid', 'residual_sugar', 'chlorides',
-                 'free_sulfur_dioxide', 'total_sulfur_dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality']
-    feature_cols = ['fixed_acidity', 'volatile_acidity', 'citric_acid', 'residual_sugar', 'chlorides',
-                    'free_sulfur_dioxide', 'total_sulfur_dioxide', 'density', 'pH', 'sulphates', 'alcohol']
+# load dataset
+# data = pd.read_csv("dataset/winequality-white.csv", sep=';', header=None, names=col_names)
+data = pd.read_csv("dataset/winequality-red.csv", header=None, names=col_names)
 
-    # load dataset
-    # data = pd.read_csv("dataset/winequality-white.csv", sep=';', header=None, names=col_names)
-    data = pd.read_csv("dataset/winequality-red.csv", header=None, names=col_names)
+X = data[feature_cols]  # Features
+y = data.quality  # Target variable
 
-    X = data[feature_cols]  # Features
-    y = data.quality  # Target variable
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
-                                                        random_state=1)
+
+def runner(k):
+    rs = 1
+
+    for i in range(k):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
+                                                            random_state=rs)
+        y_pred = decision_tree_impl(X_train, y_train, X_test)
+        print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
+        result = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+
+        print_results(result)
+
+        rs = rs + 13 * 2
+
+
+
+def decision_tree_impl(X_train, y_train, X_test):
+
 
     # Create Decision Tree classifier object
     clf = DecisionTreeClassifier(splitter="random", random_state=0)
@@ -33,12 +50,7 @@ def decision_tree_impl():
     # visualize_tree(clf, feature_cols) - graph too large.
 
     # Predict the response for test dataset
-    y_pred = clf.predict(X_test)
-    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
-
-    result = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
-
-    print_results(result)
+    return clf.predict(X_test)
 
 
 def print_results(result):
@@ -69,7 +81,7 @@ def print_results(result):
 def visualize_tree(clf, feature_cols):
     dot_data = StringIO()
     export_graphviz(clf, out_file=dot_data,
-                    filled=True, roundd=True,
+                    filled=True, round=True,
                     special_characters=True, feature_names=feature_cols,
                     class_names=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'])
     graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
@@ -78,4 +90,4 @@ def visualize_tree(clf, feature_cols):
 
 
 if __name__ == '__main__':
-    decision_tree_impl()
+    runner(10)

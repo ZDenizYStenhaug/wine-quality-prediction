33d69eb2b9b3ac18a06ad650c9b6804b04b5ad90
zeynepDYilmaz <yilmaz.zeynepdeniz@gmail.com>
2022-06-12 14:28:24 +0300

    validation set

--- random_forest_enchancement.py
+++ random_forest_enchancement.py
@@ -2,7 +2,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-
 # define column names
 col_names = ['fixed_acidity', 'volatile_acidity', 'citric_acid', 'residual_sugar', 'chlorides',
              'free_sulfur_dioxide', 'total_sulfur_dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality']
@@ -20,13 +19,17 @@ y = data.quality  # Target variable
 def runner():
     calculate_feature_importance()
 
+    # separation for validation set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_sub_train, X_validation, y_sub_train, y_validation = train_test_split(X_train, y_train, test_size=0.1)
+
 
 def calculate_feature_importance():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     clf = RandomForestClassifier()
     clf.fit(X_train, y_train)
     feature_importance = pd.DataFrame(clf.feature_importances_, index=col_names[0:-1],
-                                       columns=['importance']).sort_values('importance', ascending=False)
+                                      columns=['importance']).sort_values('importance', ascending=False)
     feature_importance.to_csv("./results/random-forest-feature-importance.csv", index=False)
 
 
